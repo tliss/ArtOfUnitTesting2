0489ceacd401b5d6da12eb9311632f7672504341
tliss <tayloraliss@gmail.com>
2019-01-07 18:06:26 +0000

    separate arrange, act, and assert stages

--- LogAn.UnitTests/LogAnalyzerTests.cs
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -9,21 +9,27 @@ namespace LogAn.UnitTests
         public void IsValidFileName_BadExtension_ReturnsFalse()
         {
             LogAnalyzer analyzer = new LogAnalyzer();
+            
             bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+            
             Assert.False(result);
         }
         [Test]
         public void IsValidLogFileName_GoodExtensionLowercase_ReturnsTrue()
         {
             LogAnalyzer analyzer = new LogAnalyzer();
+            
             bool result = analyzer.IsValidLogFileName("filewithgoodextension.slf");
+            
             Assert.True(result);
         }
         [Test]
         public void IsValidLogFileName_GoodExtensionUppercase_ReturnsTrue()
         {
             LogAnalyzer analyzer = new LogAnalyzer();
+            
             bool result = analyzer.IsValidLogFileName("filewithgoodextension.SLF");
+            
             Assert.True(result);
         }
     }
